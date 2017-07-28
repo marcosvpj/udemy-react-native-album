d8dd5c46397f7377156dabf53c417a3c6fa285f0
Marcos V. Pagelkopf Jr <marcos@marcosvpj.com.br>
2017-07-27 23:12:14 -0300

    Configure Firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common'
 
 class App extends Component {
+  componentWillMount () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCzKFJ4HuzMYeTblfJYNsKhSr2QZNrXtyo',
+      authDomain: 'auth-f28a4.firebaseapp.com',
+      databaseURL: 'https://auth-f28a4.firebaseio.com',
+      projectId: 'auth-f28a4',
+      storageBucket: 'auth-f28a4.appspot.com',
+      messagingSenderId: '312858588679'
+    });
+  }
+
   render () {
     return (
       <View>
